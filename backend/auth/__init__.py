06f667b4bc4756701ddef3e394cc96a86487c58c
co-cy <stf2003.kem@gmail.com>
2022-11-19 14:15:37 +0300

    [main] -> Remove checking active user

--- backend/auth/__init__.py
+++ backend/auth/__init__.py
@@ -51,10 +51,10 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
 
 
 async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    if current_user.is_activated:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    if current_user.is_blocked:
-        raise HTTPException(status_code=400, detail="Blocked user")
+    # if current_user.is_activated:
+    #     raise HTTPException(status_code=400, detail="Inactive user")
+    # if current_user.is_blocked:
+    #     raise HTTPException(status_code=400, detail="Blocked user")
     return current_user
 
 

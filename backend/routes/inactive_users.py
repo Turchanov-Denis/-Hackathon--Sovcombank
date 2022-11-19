04df19a3dfb22a53b3bc99e40c15aeebcd9532e7
co-cy <stf2003.kem@gmail.com>
2022-11-19 19:40:18 +0300

    [develop] -> Fix logic bug

--- backend/routes/inactive_users.py
+++ backend/routes/inactive_users.py
@@ -3,6 +3,7 @@ from backend.database import AsyncSession, get_session
 from backend.auth import get_current_active_user
 from backend.database.tables.user import User
 from sqlalchemy.future import select
+from sqlalchemy import and_
 
 router = APIRouter()
 
@@ -12,5 +13,5 @@ async def inactive_users(current_user: User = Depends(get_current_active_user),
                          db_session: AsyncSession = Depends(get_session)):
     if current_user.type != 1:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User is not Admin")
-    return (await db_session.execute(select(User.email, User.is_activated).where(User.type != 1, not User.is_activated))).all()
+    return (await db_session.execute(select(User.email, User.is_activated).where(and_(User.type != 1, not User.is_activated)))).all()
 
--- backend/routes/verified_users.py
+++ backend/routes/verified_users.py
@@ -3,6 +3,7 @@ from backend.database import AsyncSession, get_session
 from backend.auth import get_current_active_user
 from backend.database.tables.user import User
 from sqlalchemy.future import select
+from sqlalchemy import and_
 
 router = APIRouter()
 
@@ -13,4 +14,4 @@ async def verified_users(current_user: User = Depends(get_current_active_user),
     if current_user.type != 1:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User is not Admin")
     return (await db_session.execute(
-        select(User.email, User.is_blocked).where(User.type != 1, User.is_activated))).all()
+        select(User.email, User.is_blocked).where(and_(User.type != 1, User.is_activated)))).all()
